2e38f61ee00b038a0b9d6b93c6e1b84e17898e6b
Thomas BOIDUN <thomas.boidun@lumiscaphe.com>
2022-11-14 10:47:25 +0100

    step 2: initial commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const container = document.getElementById('viewer');
+container.style.width = "960px";
+container.style.height = "720px";
 
 const viewer = new LumiscapheViewer.Viewer(container, {
     server: 'https://wr.coatifactory.com',
 });
 
-viewer.setView({ mode: 'image', camera: '/360' });
+viewer.setView({ mode: 'image', camera: '/360', background: 'product' });
+viewer.setEncoder({ format: 'jpeg', quality: '80' });
+viewer.setParameters({ antialiasing: false, superSampling: 2 });
 
 viewer.load({ database: 'e9005c40-60cf-4344-b4f2-65847923faee' });
