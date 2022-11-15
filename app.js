545ffd9a173834e6a1df8919a45a432e208025f5
Thomas BOIDUN <thomas.boidun@lumiscaphe.com>
2022-11-15 09:47:26 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 import { Viewer } from './lib/Viewer';
 
-const container = document.getElementById("viewer-content");
-const form = document.getElementById('viewer-form');
+Viewer.server = 'https://wr.coatifactory.com';
+Viewer.databaseId = 'e9005c40-60cf-4344-b4f2-65847923faee';
+Viewer.container = document.getElementById("viewer-content");
+Viewer.form = document.getElementById('viewer-form');
 
-const server = 'https://wr.coatifactory.com';
-const databaseId = 'e9005c40-60cf-4344-b4f2-65847923faee';
-
-Viewer.setServer(server);
-Viewer.setDatabaseId(databaseId);
-Viewer.setContainer(container);
-Viewer.setForm(form);
+Viewer.addEventsOnForm();
+Viewer.setViewerImageURL()
